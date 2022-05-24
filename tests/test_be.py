0afdceb43a83c57922ccf8f0cd183095a4b85313
dougUCN <douglas.kt.wong@gmail.com>
2022-05-24 18:58:44 -0500

    Create github workflow

---
+++ tests/test_be.py
@@ -0,0 +1,11 @@
+"""
+Unit tests for the BE
+
+Assuming the BE server is running, queries the graphql endpoint
+
+Note that Pytest requires the filename and test functions to start with `test_*`
+"""
+
+
+def test_pass():
+    assert True
