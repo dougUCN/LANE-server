0467f847373519487514df52e48e372b2ff0e8e0
dougUCN <douglas.kt.wong@gmail.com>
2022-05-24 22:13:03 -0500

    Fix import error on unit test

--- tests/test_be.py
+++ tests/test_be.py
@@ -6,6 +6,8 @@ Assuming the BE server is running, queries the graphql endpoint
 Note that Pytest requires the filename and test functions to start with `test_*`
 """
 
+from gqlComms import listHistograms
+
 
 def test_listHistograms():
     _, response = listHistograms(isLive=False)
