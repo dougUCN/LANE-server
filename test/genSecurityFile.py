6799f07d6299f135a452eca809de339dd3924040
dougUCN <douglas.kt.wong@gmail.com>
2022-05-27 21:32:45 -0500

    Update genSecurityFile script

--- test/genSecurityFile.py
+++ test/genSecurityFile.py
@@ -6,20 +6,16 @@ import secrets, os, argparse
 
 parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument('--debug', type=bool, default=False, help=f'False for production, True for testing (default=False)')
-parser.add_argument('--rootdir', type=str, default=None, help=f'Root directory of project relative to where genSecurityFile.py was called')
+parser.add_argument('--rootdir', type=str, default=None, help=f'Where to put the security.py file')
 args = parser.parse_args()
 
 if args.rootdir and args.rootdir[-1] == '/':
     rootdir = args.rootdir[:-1]
-elif args.rootdir:
-    rootdir = args.rootdir
 else:
-    # Auto detect root directory
-    stream = os.popen('git rev-parse --show-toplevel')  # Get root directory of project
-    rootdir = stream.read()[:-1]  # Last char of stream should be newline
+    rootdir = args.rootdir
 
 
-filename = f'{rootdir}/server/nEDM_server/security.py'
+filename = f'{rootdir}/security.py'
 
 if os.path.exists(filename):
     raise FileExistsError(f'{filename} already exists')
