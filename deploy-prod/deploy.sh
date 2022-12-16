e55151a33d9159485f663ec97af09e6670b54410
dougUCN <douglas.kt.wong@gmail.com>
2022-12-15 19:03:29 -0700

    Add supervisord file and production deploy script

---
+++ deploy-prod/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Stopping supervisord"
+supervisorctl stop all
+
+echo "Jump to app folder"
+cd $HOME/LANE/LANE-server
+pwd
+
+git pull
+
+echo "Check for any dependency updates"
+source venv/bin/activate
+export https_proxy=http://proxyout.lanl.gov:8080
+pip install -r requirements.txt
+
+echo "Restart supervisord"
+supervisorctl start all
